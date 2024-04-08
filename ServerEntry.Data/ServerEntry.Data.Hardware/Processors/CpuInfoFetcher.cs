e08fd10188e4efb7a7f549736caeba308b5b44fb
Eaucooh <dynesshely@outlook.com>
2024-04-08 23:48:44 +0800

    💾 Feat: Add cpu core cache

--- ServerEntry.Data/ServerEntry.Data.Hardware/Processors/CpuInfoFetcher.cs
+++ ServerEntry.Data/ServerEntry.Data.Hardware/Processors/CpuInfoFetcher.cs
@@ -18,9 +18,9 @@ public partial class CpuInfoFetcher
 
         if (OperatingSystem.IsLinux())
         {
-            const string path = "/proc/cpuinfo";
+            const string basicInfoPath = "/proc/cpuinfo";
 
-            var info = File.ReadAllText(path);
+            var info = File.ReadAllText(basicInfoPath);
 
             ModelNameRegex().Match(info).WhenSuccess(x => result.Name = x?.Groups[1].Value);
 
@@ -45,10 +45,90 @@ public partial class CpuInfoFetcher
 
             ThreadCountRegex().Match(info).WhenSuccess(x => result.ThreadCount = int.Parse(x?.Groups[1].Value ?? "-1"));
 
+            const string cpuDevicesPath = "/sys/devices/system/cpu";
+
+            const string onlineCpusPath = cpuDevicesPath + "/online";
+
+            var onlineCpus = File.ReadAllText(onlineCpusPath).Split(',').Select(
+                x => GetItemsFromRange(GetRange(x))
+            ).SelectMany(x => x).Distinct();
+
+            result.OnlineCpus = onlineCpus;
+
+            foreach (var cpuId in onlineCpus)
+            {
+                var coreInfo = new CpuCoreInfo();
+
+                var infoDirPath = cpuDevicesPath + $"/cpu{cpuId}";
+
+                var cacheDirPath = infoDirPath + $"/cache";
+
+                var indexes = new DirectoryInfo(cacheDirPath).GetDirectories()
+                    .Where(x => IndexRegex().IsMatch(x.Name.ToLower()))
+                    .Select(x => x.Name);
+
+                foreach (var index in indexes)
+                {
+                    var sizeInfoPath = cacheDirPath + $"/{index}/size";
+
+                    var levelInfoPath = cacheDirPath + $"/{index}/level";
+
+                    var size = BinarySize.Parse(File.ReadAllText(sizeInfoPath) + "B");
+
+                    var level = int.Parse(File.ReadAllText(levelInfoPath));
+
+                    switch (level)
+                    {
+                        case 1:
+                            switch (index.Last() - '0')
+                            {
+                                case 0:
+                                    coreInfo.L1DataCacheSize = new CacheInfo()
+                                    {
+                                        Capacity = size,
+                                    };
+                                    break;
+                                case 1:
+                                    coreInfo.L1InstructionCacheSize = new CacheInfo()
+                                    {
+                                        Capacity = size,
+                                    };
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            coreInfo.L2CacheSize = new CacheInfo()
+                            {
+                                Capacity = size,
+                            };
+                            break;
+                        case 3:
+                            coreInfo.L3CacheSize = new CacheInfo()
+                            {
+                                Capacity = size,
+                            };
+                            break;
+                    }
+                }
+
+                result.CpuCoreInfos = result.CpuCoreInfos.Append(coreInfo);
+            }
+
             return result;
         }
 
         return result;
+
+        static Range GetRange(string x)
+        {
+            var se = x.Split('-');
+
+            if (se.Length != 2) throw new ArgumentException("Invalid range expression");
+
+            return new Range(int.Parse(se[0]), int.Parse(se[1]));
+        }
+
+        static IEnumerable<int> GetItemsFromRange(Range range) => Enumerable.Range(range.Start.Value, range.End.Value + 1);
     }
 
     [GeneratedRegex(@"model name\s+:\s+(.+)")]
@@ -71,4 +151,7 @@ public partial class CpuInfoFetcher
 
     [GeneratedRegex(@"siblings\s+:\s+(.+)")]
     private static partial Regex ThreadCountRegex();
+
+    [GeneratedRegex(@"index\d+")]
+    private static partial Regex IndexRegex();
 }
--- ServerEntry.Shared/Hardware/Processor/Processors/CpuInfo.cs
+++ ServerEntry.Shared/Hardware/Processor/Processors/CpuInfo.cs
@@ -14,17 +14,26 @@ public class CpuInfo : ProcessorInfo
 
     public int CoreCount { get; set; } = -1;
 
+    public IEnumerable<int> OnlineCpus { get; set; } = [];
+
     public int LogicCoreCount { get; set; } = -1;
 
     public bool Virtualizable { get; set; } = false;
 
-    public CacheInfo? L1CacheSize { get; set; }
+    public CacheInfo? TotalCacheSize { get; set; }
+
+    public IEnumerable<CpuCoreInfo> CpuCoreInfos { get; set; } = [];
+}
+
+public class CpuCoreInfo
+{
+    public double Usage { get; set; } = 0;
+
+    public CacheInfo? L1DataCacheSize { get; set; }
+
+    public CacheInfo? L1InstructionCacheSize { get; set; }
 
     public CacheInfo? L2CacheSize { get; set; }
 
     public CacheInfo? L3CacheSize { get; set; }
-
-    public CacheInfo? TotalCacheSize { get; set; }
-
-    public IEnumerable<double> PerCoreUsage { get; set; } = [];
 }
