3db6ab540fb9416e51ae8e20158a93488c85061e
Eaucooh <dynesshely@outlook.com>
2024-04-28 22:28:17 +0800

    💾 Feat(Backend): Redirect "/" -> swagger docs

---
+++ ServerEntry.ApiServer/Controllers/V1/Navigation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerEntry.ApiServer.Controllers.V1;
+
+[ApiController]
+[Route("/")]
+public class Navigation(ILogger<Navigation> logger) : ControllerBase
+{
+    private readonly ILogger<Navigation> _logger = logger;
+
+    [HttpGet("", Name = nameof(Redirect))]
+    public IActionResult Redirect()
+    {
+        return Redirect("swagger/index.html");
+    }
+}
--- ServerEntry.ApiServer/Program.cs
+++ ServerEntry.ApiServer/Program.cs
@@ -1,14 +1,8 @@
-var AllowSpecificOriginsPolicyName = "AllowAllOrigins";
+using ServerEntry.ApiServer.Utils.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(
-        AllowSpecificOriginsPolicyName,
-        policy => policy.AllowAnyOrigin().AllowAnyMethod()
-    );
-});
+builder.Services.AllowAllOrigins();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -16,11 +10,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Even in production environment, provides swagger api document unless user turn it off
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseCors(AllowSpecificOriginsPolicyName);
+app.AllowAllOrigins();
 
 app.UseHttpsRedirection();
 
